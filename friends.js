adde6e88369f3aaec810b11950d45b546c3eff37
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-06-09 16:52:33 -0600

    friend.js

---
+++ friends.js
@@ -0,0 +1,17 @@
+var numbers = [1,22,35,24,59,46,7,82]
+
+for (var i = 0; i < numbers.length; i++ )  {
+
+  if (numbers[i] > 30 )
+
+   console.log (numbers[i])  }
+
+console.log("________________")
+
+numbers = [1,22,35,24,59,46,7,82]
+
+for (var i = 0; i < numbers.length; i++ )  {
+
+  if (numbers[i] < 60 )
+
+   console.log (numbers[i])  }
